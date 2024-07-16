756b8aea250ab9d03f94227cba5bbe4dd69011b8
Sofia Berlin <sofiamberlin@gmail.com>
2024-07-15 21:43:13 -0700

    Add Footer content

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -2,22 +2,8 @@ function Footer() {
   return (
     <footer className="bg-light-color rounded-lg shadow dark:bg-gray-800">
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://github.com/sofiamay/" className="hover:underline">Flowbite™</a>. All Rights Reserved.
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">©2024 Website by <a href="https://github.com/sofiamay/" className="hover:underline">Sofia May</a>
         </span>
-        {/* <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-            <li>
-                <a href="#" class="hover:underline me-4 md:me-6">About</a>
-            </li>
-            <li>
-                <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
-            </li>
-            <li>
-                <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
-            </li>
-            <li>
-                <a href="#" class="hover:underline">Contact</a>
-            </li>
-        </ul> */}
       </div>
     </footer>
   );
