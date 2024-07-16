b110422bbf3b3ed2cbcb17826a1c048120e5b02c
Sofia Berlin <sofiamberlin@gmail.com>
2024-07-15 17:58:37 -0700

    Add auth/token endpoint

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const config = {
   clientId: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   authUrl: 'https://accounts.google.com/o/oauth2/v2/auth', //link to the google auth consent screen
-  tokenUrl: 'https://oauth2.googleapis.com/token', //link to very authorization code and  get token
+  tokenUrl: 'https://oauth2.googleapis.com/token', //link to very authorization code from front end and get token
   redirectUrl: process.env.REDIRECT_URL,
   clientUrl: process.env.CLIENT_URL,
   tokenSecret: process.env.TOKEN_SECRET,
@@ -44,7 +44,7 @@ const getTokenParams = (code) =>
 
 const app = express();
 
-/* Middleware */
+/* -- Middleware -- */
 
 // CORS
 app.use(
@@ -70,3 +70,48 @@ const auth = (req, res, next) => {
     return res.status(401).json({ message: 'Unauthorized' });
   }
 };
+
+/* -- Routes -- */
+
+// Give the front end the google auth url with params
+app.get('/auth/url', (_, res) => {
+  res.json({
+    url: `${config.authUrl}?${authParams}`,
+  });
+});
+
+// get an authorization code, verifies it with google
+// and sends back a token to the client cookies along with user info
+app.get('auth/token', async (req, res) => {
+  const { code } = req.query;
+  if (!code) return res.status(400).json({ message: 'Code is required' });
+  try {
+    // get params required to get token from google
+    const tokenParams = getTokenParams(code);
+    // exchange code for token
+    const {
+      data: { id_token },
+    } = await axios.post(`${config.tokenUrl}?${tokenParams}`);
+    // 400 error if invalid code
+    if (!id_token) return res.status(400).json({ message: 'Invalid auth code' });
+    // get user info from id token
+    const { name, email, picture } = jwt.decode(id_token);
+    const user = { name, email, picture };
+    // sign new token
+    const token = jwt.sign({ user }, config.tokenSecret, {
+      expiresIn: config.tokenExpiration,
+    });
+    // set cookies to sent to user
+    res.cookie('token', token, {
+      httpOnly: true,
+      secure: true,
+      maxAge: config.tokenExpiration,
+    });
+    // send user back to client
+    res.json({ user });
+  } catch (error) {
+    // 400 error if invalid code
+    console.error('Error: ', error);
+    res.status(400).json({ message: 'Invalid auth code' });
+  }
+});
