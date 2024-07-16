d98533a3895d780841a560f2c2041c8ec2e5eb99
Sofia Berlin <sofiamberlin@gmail.com>
2024-07-15 22:28:15 -0700

    Create Login component

--- client/src/routes/home.tsx
+++ client/src/routes/home.tsx
@@ -1,14 +1,12 @@
 import MainLayout from "../layouts/MainLayout";
 import { SITE_TITLE } from "../constants";
+import Login from "../components/Login";
 
 function Home() {
   const title = SITE_TITLE;
   return (
     <MainLayout title={title}>
-      <div className="flex flex-col justify-center">
-        <h1>Home</h1>
-        <p>Welcome to the home page!</p>
-      </div>
+      <Login />
     </MainLayout>
   );
 }
